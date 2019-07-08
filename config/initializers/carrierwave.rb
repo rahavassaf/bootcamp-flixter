d2e49865eb36781e5167d0c8f9627bcbc765c9d3
Assaf Rahav <rahav.assaf@gmail.com>
2019-07-08 05:13:08 +0000

    fog timeout

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,6 +17,11 @@ CarrierWave.configure do |config|
     config.fog_directory = 'flixter-assaf-rahav'
 
     config.fog_public = false
+
+    config.fog_attributes = {
+      multipart_chunk_size: 2097152, # 2MiB
+      write_timeout: 600, # 10 minutes
+    }
   else
     config.storage = :file
   end
