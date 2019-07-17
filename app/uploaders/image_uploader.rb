1af39d2fcfe63f460f8b938f1590fc1be327def0
Assaf Rahav <rahav.assaf@gmail.com>
2019-07-17 03:30:34 +0000

    more image stuff

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-  process resize_to_fill: [800, 350]
+  process resize_to_fill: [2000, 750]
 
   # Create different versions of your uploaded files:
   # version :thumb do
